9626a085cbfdcf618ab1738ff8f5d3593a213a8b
Shean-D <sheandivyan01@gmail.com>
2025-03-15 18:03:23 +0530

    Login page linked & register page design begins

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,5 +1,23 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class LoginFormController {
 
+    @FXML
+    private Button btnBack;
+
+    public void btnBackOnAction() throws IOException {
+
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Welcome.fxml"))));
+        stage.setTitle("Welcome Page");
+        stage.show();
+
+    }
 }
--- src/main/java/controller/WelcomeFormController.java
+++ src/main/java/controller/WelcomeFormController.java
@@ -2,10 +2,15 @@ package controller;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,9 +20,12 @@ public class WelcomeFormController {
     @FXML
     private TextField txtTime;
 
+    public void initialize() {
+        txtTimeClickOnAction();
+    }
 
     public void txtTimeClickOnAction() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ");
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
             LocalDateTime now = LocalDateTime.now();
@@ -28,5 +36,13 @@ public class WelcomeFormController {
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
 
+    }
+
+    public void btnLogonOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login.fxml"))));
+        stage.setTitle("Login Page");
+        stage.show();
     }
 }
